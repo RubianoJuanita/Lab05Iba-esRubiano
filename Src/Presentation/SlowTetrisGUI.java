c094ee794b6bfe1e642c778de013874b15d7d2d2
RubianoJ <juanita.rubiano-f@mail.escuelaing.edu.co>
2025-11-15 19:33:26 -0500

    oyente de ciclo0 funcional

--- Src/Presentation/SlowTetrisGUI.java
+++ Src/Presentation/SlowTetrisGUI.java
@@ -1,20 +1,48 @@
+
 import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowAdapter;
 import javax.swing.*;
 
 public class SlowTetrisGUI extends JFrame {
-/*... */
-private SlowTetrisGUI(){;
-    prepareElementos();
-}
-public static void main(String[] args ){
-    SlowTetrisGUI gui = new SlowTetrisGUI();
-    gui.setVisible(true);
-    gui.setLocationRelativeTo(null);
-}
-private void prepareElementos(){
-    setTitle("Slow Tetris");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    Dimension s =Toolkit.getDefaultToolkit().getScreenSize();
-    setSize(s.width/2, s.height/2);
-}
+    /* ... */
+    private SlowTetrisGUI() {
+        prepareElementos();
+        prepareActions();
+    }
+
+    public static void main(String[] args) {
+        SlowTetrisGUI gui = new SlowTetrisGUI();
+        gui.setVisible(true);
+        gui.setLocationRelativeTo(null);
+    }
+
+    private void prepareElementos() {
+        setTitle("Slow Tetris");
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        Dimension s = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(s.width / 2, s.height / 2);
+    }
+
+    private void prepareActions() {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                exit();
+            }
+        });
+    }
+
+    private void exit() {
+        int confirm = JOptionPane.showConfirmDialog(
+                this,
+                "¿Esta sguro de que desea salir del juego?",
+                "Confirmar Salida",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE);
+
+        if (confirm == JOptionPane.YES_OPTION) {
+            System.exit(0);
+        }
+    }
 }
